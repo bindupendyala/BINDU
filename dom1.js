87b436f8405693f7bcd3874631861cd270c53225
Bindu Madhavi Pendyala <97437507+bindupendyala@users.noreply.github.com>
2023-08-29 10:11:06 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,33 @@
+// // let p1= document.getElementById('a');
+// // p1.style.fontSize = '50px';
+// // let p2= document.getElementById('b');
+// // p2.style.color = 'red';
+// // let img1=document.getElementById('c');
+// // img1.src="../images/travel.jpeg";
+// // let p3=document.getElementById('d')
+// // p3.style.border = '3px solid red';
+// // let p4=document.getElementById('e');
+// // p4.innerHTML = 'please enter name';
+// // p4.style.color = 'red';
+// let p5=document.getElementsByClassName('a');
+// p5[0].innerHTML='Hyper Text Markup Language';
+// p5[0].style.color='darkblue';
+// p5[2].innerHTML='JAVASCRIPT';
+// p5[2].style.color='purple';
+// let p6=document.getElementsByTagName('li');
+// p6[0].innerHTML='Aditya Engneering College';
+// p6[0].style.color='purple';
+// p6[2].innerHTML='Aditya College of Engneering';
+// p6[2].style.color='green';
+// document.write("<table border = 3px solid black>")
+// for (rows =1; rows<=5; rows++){
+//         document.write("</tr>")
+//         for(col=1; col<=7;col++){
+
+//                 document.write("<td style = padding:10px>" +col+ "<br>" + "</td>")
+// }
+// document.write("</tr>")
+// }
+let p= document.querySelector('.a');
+p.innerText='hyper text markup';
+p.innerText='cascading style sheets';
